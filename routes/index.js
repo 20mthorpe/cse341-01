dc18bdedff966caa16d6d5f841af349be84774ee
20mthorpe <tho20019@byui.edu>
2024-04-26 17:03:12 -0600

    add maria route

--- routes/index.js
+++ routes/index.js
@@ -3,5 +3,6 @@ const l01Controller = require('../controllers/lesson01');
 
 routes.get('/', l01Controller.maryRoute);
 routes.get('/jenny', l01Controller.jennyRoute);
+routes.get('/maria', l01Controller.mariaRoute);
 
 module.exports = routes;
