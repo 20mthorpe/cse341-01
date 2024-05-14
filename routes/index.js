68ec351229722d18dbf87c7e7bb132623222a440
20mthorpe <tho20019@byui.edu>
2024-05-14 11:05:03 -0600

    swagger

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,16 @@
 const router = require('express').Router();
 const l01Controller = require('../controllers/lesson01');
 
-router.get('/', l01Controller.maryRoute);
+const swaggerUi = require('swagger-ui-express');
+const swaggerDocument = require('../swagger-output.json');
+
+router.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+
+router.use('/', require('./contacts'));
 // routes.get('/jenny', l01Controller.jennyRoute);
 // routes.get('/maria', l01Controller.mariaRoute);
 
-router.use('/contacts', require('./contacts'));
-router.use('/contacts/:id', require('./contacts'));
+//router.use('/contacts', require('./contacts'));
+//router.use('/contacts/:id', require('./contacts'));
 
 module.exports = router;
